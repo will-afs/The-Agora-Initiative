61136098e09be82c4851dda55a13f1c54e510378
William AFONSO <william.afonso.pro@gmail.com>
2021-09-04 11:36:44 +0200

    - Implemented Logout with tests (logout success, logout unauthenticated fails, logout but token expired fails)

--- backend/account/api/urls.py
+++ backend/account/api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from account.api.views import(
     registration_view,
+    Logout
 )
 
 from rest_framework.authtoken.views import obtain_auth_token
@@ -9,5 +10,6 @@ app_name = 'account'
 
 urlpatterns = [
     path('register/', registration_view, name='register'),
-    path('login/', obtain_auth_token, name='login')
+    path('login/', obtain_auth_token, name='login'),
+    path('logout/', Logout.as_view(), name='logout'),
 ]
